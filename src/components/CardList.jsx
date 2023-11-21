d2cc2319a66477a2b23be5f6053b462aae67e0e0
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2023-11-21 12:55:53 +0100

    react-use-error-boundary added to cardlist

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,7 +1,25 @@
+import { withErrorBoundary, useErrorBoundary } from "react-use-error-boundary";
+
 import Card from "./Card.jsx";
 
-export default function CardList({ robots }) {
-  return (
+const CardList = withErrorBoundary(({ robots }) => {
+  const [error, resetError] = useErrorBoundary();
+
+  return error ? (
+    <>
+      <div className="space-y-10">
+        <h1 className="font-mono text-[#0ccac4] text-6xl font-bold uppercase">
+          {error.message}
+        </h1>
+        <button
+          onClick={resetError}
+          className="border-[#0ccac4] rounded-md px-6 py-2 border-4 font-mono text-[#0ccac4] text-6xl font-bold uppercase"
+        >
+          Try Again
+        </button>
+      </div>
+    </>
+  ) : (
     <>
       <div className="grid place-content-center grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(15rem,_20rem))] gap-5">
         {robots.map((robot, i) => {
@@ -17,4 +35,6 @@ export default function CardList({ robots }) {
       </div>
     </>
   );
-}
+});
+
+export default CardList;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,37 +2,24 @@ import { useState, useEffect } from "react";
 import Scroll from "../components/Scroll.jsx";
 import CardList from "../components/CardList.jsx";
 import SearchBox from "../components/SearchBox.jsx";
-import ErrorBoundary from "../components/ErrorBoundary.jsx";
 
 export default function App() {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: "",
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((users) => this.setState({ robots: users }));
-  // }
-
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  }, []);
 
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
   };
 
-  // const { robots, searchfield } = this.state;
-
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   });
-
-  console.log(robots, searchfield);
 
   return !robots.length ? (
     <>
@@ -56,9 +43,7 @@ export default function App() {
             </h1>
             <SearchBox searchChange={onSearchChange} />
             <Scroll>
-              <ErrorBoundary>
-                <CardList robots={filteredRobots} />
-              </ErrorBoundary>
+              <CardList robots={filteredRobots} />
             </Scroll>
           </div>
         </section>
