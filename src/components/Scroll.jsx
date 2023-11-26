fb97c440af23e834f9d7992cf8dd9c5dfd02a975
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2023-11-26 12:59:11 +0100

    scroll component changed

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,8 +1,8 @@
-export default function Scroll(props) {
+export default function Scroll({ children }) {
   return (
     <>
       <div className="overflow-y-scroll border-8 border-black rounded-lg h-[70vh] p-5">
-        {props.children}
+        {children}
       </div>
     </>
   );
